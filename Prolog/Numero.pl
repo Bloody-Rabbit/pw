aa221067435b6137a503a2b21b17079a228e2e61
Bloody-Rabbit <Samantthaify@gmail.com>
2017-10-04 16:06:45 -0600

    laloa

---
+++ Prolog/Numero.pl
@@ -0,0 +1,3 @@
+%Reescriba el siguiente programa sin usar ;
+
+traduce(Numero, Palabra) :- Numero = 1, Palabra = uno; Numero = 2, Palabra = dos; Numero = 3, Palabra = tres; 
---
+++ Prolog/SegundoElementoLista.pl
@@ -0,0 +1,3 @@
+%Obtener si el segundo elemento de una lista es X
+
+segundo(X, [_, X | _]).
