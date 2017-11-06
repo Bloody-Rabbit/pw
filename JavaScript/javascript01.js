665c43e4aee361ebdb2840ff1516fdc6d1e3f200
Bloody-Rabbit <Samantthaify@gmail.com>
2017-11-06 15:57:49 -0700

    javascript

---
+++ JavaScript/javascript01.js
@@ -0,0 +1,13 @@
+var datos = function ()
+	{
+
+		var nombre;
+		var edad;
+		nombre = prompt ("Teclea tu nombre");
+		edad   = prompt ("Teclea tu edad");
+		Document.write ("Hola");
+		Document.write (nombre);
+		Document.write ("Tienes");
+		Document.write (edad);
+		Document.write ("años");
+	}
